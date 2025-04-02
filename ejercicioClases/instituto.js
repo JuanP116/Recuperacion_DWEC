f2dfd84a0a91183911abe9e34ca61df7fb3fc97e
DESKTOP-NQO1TN8\juan pedro <juanpferrete48@gmail.com>
2025-04-02 18:52:34 +0200

    clase alumno

---
+++ ejercicioClases/instituto.js
@@ -0,0 +1,56 @@
+// Escribe aquí tu código
+
+let Alumno = class {
+    constructor(codigo, nombre, apellido1, apellido2, dni) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.dni = dni;
+        this.notas = [];
+    }
+
+    agregarNotas(...notas) {
+        this.notas.push(...notas);
+    }
+
+    obtenerCorreoCorporativo(dominio) {
+        let dniSinLetra = this.dni.slice(0, -1);
+        const nombreUsuario = this.nombre.charAt(0).toLowerCase() + this.apellido1.toLowerCase().slice(0, 3) + this.apellido2.toLowerCase().slice(0, 3) + dniSinLetra.slice(-3 );
+        return `${nombreUsuario}@${dominio}`;
+    }
+
+    obtenerNotasMedias() {
+        const notasMedias = {};
+        this.notas.forEach(nota => {
+            if (!notasMedias[nota.asignatura]) {
+                notasMedias[nota.asignatura] = [];
+            }
+            notasMedias[nota.asignatura].push(nota.nota);
+        });
+        for (const asignatura in notasMedias) {
+            const suma = notasMedias[asignatura].reduce((a, b) => a + b, 0);
+            const media = suma / notasMedias[asignatura].length;
+            notasMedias[asignatura] = media;
+        }
+        return notasMedias;
+    }
+
+
+}
+
+let Aula = class {
+    constructor(numeroAula, nombreAula) {
+        this.numeroAula = numeroAula;
+        this.nombreAula = nombreAula;
+        this.alumnos = [];  
+    }
+
+    agregarAlumno(alumno) {
+        this.alumnos.push(alumno);
+    }
+
+    get numeroAlumnos() {
+        return this.alumnos.length;
+    }
+}
